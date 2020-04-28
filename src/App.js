06dde2ee86328832a35fd115e25fbca5f1aa05d9
kunstix <scroovy@web.de>
2020-04-28 20:59:44 +0200

    adding shop collectionDD

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 import Shop from './pages/Shop';
-import './App.css';
+import Header from './components/header/Header';
+import './app.css';
 
 function App() {
   return (
     <div className='App'>
-      <Route exact path='/' component={Home} />
-      <Route exact path='/shop' component={Shop} />
+      <Header />
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/shop' component={Shop} />
+      </Switch>
     </div>
   );
 }
