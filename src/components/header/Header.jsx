7e02640adb98f5df3f5ea9b9a2126f993b09110e
kunstix <scroovy@web.de>
2020-04-30 23:02:21 +0200

    adding cart

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { auth } from '../../firebase/firebaseUtils';
-
+import CartIcon from '../header/CartIcon';
+import CartDropdown from '../header/CartDropDown';
 import { ReactComponent as Logo } from '../../assets/muffin.svg';
 import './header.scss';
 
-const Header = ({ currentUser }) => {
+const Header = ({ currentUser, hidden }) => {
   return (
     <div className='header'>
       <Link className='logo-container' to='/'>
@@ -28,15 +29,16 @@ const Header = ({ currentUser }) => {
             SIGN IN
           </Link>
         )}
+        <CartIcon />
       </div>
+      {hidden ? null : <CartDropdown />}
     </div>
   );
 };
 
-const mapStateToProps = state => {
-  return {
-    currentUser: state.user.currentUser
-  };
-};
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden
+});
 
 export default connect(mapStateToProps)(Header);
---
+++ src/redux/actions/cartActions.js
@@ -0,0 +1,5 @@
+import { TOGGLE_CART_HIDDEN } from './types';
+
+export const toggleCartHidden = () => {
+  return { type: TOGGLE_CART_HIDDEN };
+};
